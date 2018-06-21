124dea73520b1ce9233d5841159bb75d445255d1
Rouz Majlessi <rmajlessi@gmail.com>
2018-06-20 19:16:19 -0700

    register Identity to services and create user context file associated with it

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -33,6 +34,8 @@ namespace quiz_backend
             }));
 
             services.AddDbContext<QuizContext>(opt => opt.UseInMemoryDatabase("quiz"));
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<UserDbContext>();
 
             services.AddMvc();
         }
